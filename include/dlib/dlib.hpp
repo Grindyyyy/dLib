3521bfff9bc300196f2537690851cc14f94e8b6c
Grindyyyy <grindyyyyyyyyyy@gmail.com>
2024-09-26 13:18:43 -0400

    9/26/24 Odometry v1.0.0 Addition to dLib

--- include/dlib/dlib.hpp
+++ include/dlib/dlib.hpp
@@ -7,6 +7,9 @@
 
 namespace dlib {
 
+bool odom_started = false;
+std::unique_ptr<pros::Task> odom_updater;
+
 template<typename Robot>
 void set_mode_brake(Robot& robot){
     robot.get_chassis().left.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
@@ -26,6 +29,44 @@ void set_mode_hold(Robot& robot){
 }
 
 template<typename Robot>
+void update_odom(Robot& robot){
+    while(true){
+        robot.get_odom().update(get_motor_inches(robot), get_imu_heading(robot) * (M_PI / 180.00)); 
+        pros::delay(10);
+    }
+}
+
+template<typename Robot>
+void calibrate(Robot& robot){
+    robot.get_chassis().left.set_gearing(pros::E_MOTOR_GEAR_BLUE);
+    robot.get_chassis().right.set_gearing(pros::E_MOTOR_GEAR_BLUE);
+
+    robot.get_chassis().left.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
+    robot.get_chassis().right.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
+
+    robot.get_chassis().left.tare_position();
+    robot.get_chassis().right.tare_position();
+
+    robot.get_imu().imu.reset(true);
+}
+
+template<typename Robot>
+void start_odom(Robot& robot) {
+    // Capture the current class instance and call update_odom in a seperate task
+
+    if (!odom_started) {
+        odom_updater = std::make_unique<pros::Task>([&] { update_odom(robot); });
+        odom_started = true;
+    }
+}
+
+template<typename Robot>
+void tare_position(Robot& robot){
+    robot.get_chassis().left.tare_position();
+    robot.get_chassis().right.tare_position();
+}
+
+template<typename Robot>
 void brake_motors(Robot& robot)  {
     robot.get_chassis().left.brake();
     robot.get_chassis().right.brake();
@@ -35,6 +76,11 @@ template<typename Robot>
 void move_voltage(Robot& robot, std::int32_t power) {
     robot.get_chassis().left.move(power);
     robot.get_chassis().right.move(power);
+}
+template<typename Robot>
+void move_voltage(Robot& robot, std::int32_t left_volts, std::int32_t right_volts) {
+    robot.get_chassis().left.move(left_volts);
+    robot.get_chassis().right.move(right_volts);
 }
 
 // Turn at a given voltage
@@ -47,8 +93,8 @@ void turn_voltage(Robot& robot, std::int32_t power) {
 // Drive using the arcade scheme
 template<typename Robot>
 void arcade(Robot& robot, std::int8_t power, std::int8_t turn) {
-    robot.get_chassis().left.move(power + turn);
-    robot.get_chassis().right.move(power - turn);
+        robot.get_chassis().left.move((power - turn));
+        robot.get_chassis().right.move(power + turn);
 }
 
 // Constrain the angle to -180 to 180 for efficient turns
@@ -69,7 +115,6 @@ double get_imu_heading(Robot& robot) {
     double ccw = std::fmod(360.0 - robot.get_imu().get_heading(), 360.0);
     return angle_within_180(ccw);
 }
-
 
 // Get the average inches the drivebase motors have moved
 template<typename Robot>
@@ -183,6 +228,83 @@ void turn_degrees(Robot& robot, double angle, Options options) {
     }
 
     brake(robot);
+}
+
+template<typename Robot>
+// Calculate the angle to turn from the current position to a point
+double angle_to(Robot& robot, double x, double y, bool reverse){
+    double target_x = x;
+    double target_y = y;
+
+    Position new_position = robot.get_odom().odom.get_position();
+    double current_x = new_position.x;
+    double current_y = new_position.y;
+
+    double angle = std::atan2(target_y - current_y, target_x - current_x) * (180.0 / M_PI);
+
+    // If reverse is true add 180 so that the bot faces the opposite direction
+    if(reverse){
+        angle += 180;
+    }
+
+    return angle_within_180(angle);
+}
+
+template<typename Robot>
+// Calculate the distance from the current position to a given point
+double dist_to(Robot& robot, double x, double y, bool reverse){
+    double target_x = x;
+    double target_y = y;
+
+    Position new_position = robot.get_odom().odom.get_position();
+    double current_x = new_position.x;
+    double current_y = new_position.y;
+    
+    double dist = std::hypot(target_x - current_x, target_y - current_y);
+    // The bot will move in the opposite direction
+    if(reverse){
+        dist = -dist;
+    }
+
+    return dist;
+}
+
+// Get the robot's current x, y and theta
+template<typename Robot>
+Position get_position(Robot& robot, bool radians) {
+    Position position = robot.get_odom().odom.get_position();
+
+    if (!radians) {
+        position.theta *= 180.0 / M_PI;
+    }
+
+    return position;
+}
+
+// Set the robot's current x, y and theta
+template<typename Robot>
+void set_position(Robot& robot, Position new_position, bool radians) {
+    if (radians) {
+        new_position.theta *= M_PI / 180.0;
+    }
+
+    robot.get_odom().odom.set_position(new_position);
+}
+
+
+template<typename Robot>
+// Turn to a coordinate point using Odometry and PID
+void turn_to(Robot& robot, double x, double y, bool reverse){
+    double target_angle = angle_to(robot, x, y, reverse);
+    turn_degrees(robot, target_angle);
+}
+
+template<typename Robot>
+// Move to a coordinate point using Odometry and PID
+void move_to(Robot& robot, double x, double y, bool reverse) {
+    turn_to(robot, x, y, reverse);
+    double dist = dist_to(robot, x, y, reverse);
+    move_inches(robot, dist);
 }
 
 }
